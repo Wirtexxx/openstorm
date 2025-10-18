f69989efddfcdecfdb63bb9618d5c7cf10857a3a
Vlad Haidaichuk <vlad.haidaichuk@gmail.com>
2025-10-18 17:22:07 +0200

    fix poetry.toml

--- scripts/build_installer.sh
+++ scripts/build_installer.sh
@@ -9,17 +9,28 @@ BUILD_DIR="../.build"
 rm -rf "$BUILD_DIR" "$DIST_DIR"
 
 echo "Building executable..."
-pyinstaller --onefile ../openstorm/__main__.py \
+source ../.venv/bin/activate
+
+# build with pyinstaller
+pyinstaller --onefile -m ../openstorm \
             --name "$APP_NAME" \
             --distpath "$BUILD_DIR/dist" \
             --workpath "$BUILD_DIR/build" \
-            --specpath "$BUILD_DIR/spec"
+            --specpath "$BUILD_DIR/spec" \
+            --hidden-import=typer \
+            --hidden-import=pydantic_settings \
+            --hidden-import=pymongo \
+            --hidden-import=pymongo.errors \
+            --hidden-import=rich \
+            --hidden-import=rich.markup
 
-echo "Creating source archive..."
+#mkdir -p "$DIST_DIR"
+#
+#mv "$BUILD_DIR/dist/$APP_NAME" "$DIST_DIR/"
+
 mkdir -p "$BUILD_DIR/$DIST_DIR"
-tar -czf "$BUILD_DIR/$DIST_DIR/${APP_NAME}-${VERSION}.tar.gz" ../openstorm scripts README.md pyproject.toml
-
-mv "$BUILD_DIR/dist/$APP_NAME" "$DIST_DIR/"
+tar -czf "$BUILD_DIR/$DIST_DIR/${APP_NAME}-${VERSION}.tar.gz" \
+    -C .. openstorm scripts README.md pyproject.toml
 
 echo "Build completed. Files in $DIST_DIR:"
 ls -l "$DIST_DIR"
